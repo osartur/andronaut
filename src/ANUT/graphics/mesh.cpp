6db890b34decda5a0b45978a1f0884c357927e8a
osartur <dantasartur50@gmail.com>
2024-02-09 14:55:23 -0300

    ignore

--- src/ANUT/graphics/mesh.cpp
+++ src/ANUT/graphics/mesh.cpp
@@ -11,8 +11,16 @@ Mesh::~Mesh()
 	Free();
 }
 
-void Mesh::Alloc(int vertex_count, int index_count, const unsigned layout[4], unsigned usage)
+void Mesh::Alloc(int vertex_count, int index_count, const unsigned attribs_count[4], unsigned usage)
 {
+	vertex_size = 0;
+	int attribs_size = 0;
+	for (int i = 0; i < 4 && attribs_count[i] != 0; i++)
+	{
+		vertex_size += attribs_count[i] * sizeof(float);
+		attribs_size++;
+	}
+	
 	glGenVertexArrays(1, &vao);
 	glGenBuffers(1, &vbo);
 	glGenBuffers(1, &ibo);
@@ -20,20 +28,14 @@ void Mesh::Alloc(int vertex_count, int index_count, const unsigned layout[4], un
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
 	
-	vertex_size = 0;
-	for (int i = 0; i < 4; i++)
-	{
-		vertex_size += layout[i] * sizeof(float);
-	}
-	
-	for (int i=0, begin=0; i < 4 && layout[i] != 0; begin+=layout[i]*sizeof(float), i++)
-	{
-		glEnableVertexAttribArray(i);
-		glVertexAttribPointer(i, layout[i], GL_FLOAT, false, vertex_size, (void*) begin);
-	}
-	
 	glBufferData(GL_ARRAY_BUFFER, vertex_count * vertex_size, nullptr, usage);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, index_count * sizeof(unsigned), nullptr, usage);
+	
+	for (int i=0, begin=0; i < attribs_size; begin+=attribs_count[i]*sizeof(float), i++)
+	{
+		glEnableVertexAttribArray(i);
+		glVertexAttribPointer(i, attribs_count[i], GL_FLOAT, false, vertex_size, (void*) begin);
+	}
 	
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
@@ -43,17 +45,21 @@ void Mesh::Alloc(int vertex_count, int index_count, const unsigned layout[4], un
 void Mesh::CopyData(const void* data, int vertex_count, const unsigned* index, int index_count, int i)
 {
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
 	glBufferSubData(GL_ARRAY_BUFFER, i * vertex_size, vertex_count * vertex_size, data);
-	glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, i * sizeof(unsigned), index_count * sizeof(unsigned), index);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, i * sizeof(unsigned), index_count * sizeof(unsigned), index);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 void Mesh::Draw(unsigned primitive, int count)
 {
 	glBindVertexArray(vao);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
 	glDrawElements(primitive, count, GL_UNSIGNED_INT, (void*) 0);
+	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 void Mesh::Free()
--- src/ANUT/graphics/mesh.h
+++ src/ANUT/graphics/mesh.h
@@ -7,7 +7,7 @@ public:
 	Mesh();
 	~Mesh();
 	
-	void Alloc(int vertex_count, int index_count, const unsigned layout[4], unsigned usage);
+	void Alloc(int vertex_count, int index_count, const unsigned attribs_count[4], unsigned usage);
 	void CopyData(const void* data, int vertex_count, const unsigned* index, int index_count, int i = 0);
 	void Draw(unsigned primitive, int count);
 	void Free();
