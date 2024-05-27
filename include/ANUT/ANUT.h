4f2c2bff9ec328e81f0018231bd287373b8ff348
osartur <dantasartur50@gmail.com>
2024-05-27 20:16:07 -0300

    add ANUT_opengles.h

--- include/ANUT/ANUT.h
+++ include/ANUT/ANUT.h
@@ -1,12 +1,7 @@
 #if not defined(ANDROID_NATIVE_UTILITY_TOOLKIT_H)
 #define ANDROID_NATIVE_UTILITY_TOOLKIT_H
 
-#include <GLES3/gl32.h>
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
-
 #include "android_native_app_glue.h"
-
 #include "activity.h"
 #include "engine.h"
 #include "event.h"
---
+++ include/ANUT/ANUT_opengles.h
@@ -0,0 +1,6 @@
+#if not defined(ANUT_OPENGLES_H)
+#define ANUT_OPENGLES_H
+
+#include <GLES3/gl32.h>
+
+#endif
